547f7cfbf82a63d5a01d9f9f520de5d811b58dcb
ftdjsox <xiomxiom@icloud.com>
2025-09-28 19:28:17 +0200

    Initial Commit

---
+++ Air Volume/Air_VolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Air_VolumeApp.swift
+//  Air Volume
+//
+//  Created by Francesco on 28/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Air_VolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
